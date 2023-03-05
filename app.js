cb9b657a90998e4514a175b283bda688b1492f08
alex4jdm <alex4jdm@gmail.com>
2023-03-05 22:06:00 +0200

    Added role system.

--- app.js
+++ app.js
@@ -7,46 +7,46 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-function getDefaultPrompt(question) {
-  return `Думай как Русский ватник. Задавай встречный вопрос и старайся вести дискуссию со мной.
+function getDefaultPrompt(title, question) {
+  return `Думай как ${title}. Задавай встречный вопрос и старайся вести дискуссию со мной.
 
   Не нужно в начале писать что ты искусственный интеллект.
-  
-  Ограничь свой ответ двумя предложениями.
 
-  Добавь глупых ошибок в текст, которые покажут неграмотность ватников.
-
-  Сформируй ответ на вопрос "${question}", а также создай встречный вопрос на эту тему.`;
+  Ответь на вопрос "${question}", а также создай встречный вопрос на эту тему.`;
 }
 
 (async function () {
   let question = "Чей Крым?";
+  let title1 = "Ватник";
+  let title2 = "Либерал";
 
-  const req = async function (question) {
+  const req = async function (title, question) {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
+      max_tokens: 400,
       temperature: 0.5,
       messages: [
         {
-          content: getDefaultPrompt(question),
+          content: getDefaultPrompt(title, question),
           role: "user",
         },
       ],
     });
-    const answer = completion.data.choices[0].message.content
-      .split(/.|\?/)
-      .map((item) => item.replaceAll("\n", ""));
-    console.log(completion.data.choices[0].message.content);
-    fs.appendFileSync("data.txt", answer.toString() + "\n");
+    const answer = completion.data.choices[0].message.content.replaceAll(
+      "\n",
+      " "
+    );
+    console.log(`- ${title}: ${answer.toString()}`);
+    fs.appendFileSync("data.txt", `- ${title}: ${answer.toString()}\n`);
 
     return new Promise((resolve, reject) => {
       let t = 0;
       setTimeout(async () => {
-        const data = await req(answer[1]);
+        const data = await req(title === title1 ? title2 : title1, answer);
         resolve(data);
       }, 10000);
     });
   };
 
-  await req(question);
+  await req(title1, question);
 })();
