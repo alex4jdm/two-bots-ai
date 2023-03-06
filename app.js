27bf08499a44188edaff6919b71c23a248fe7a39
alex4jdm <alex4jdm@gmail.com>
2023-03-06 13:07:40 +0200

    Quality fixes, added personalities and history.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
-const fs = require("fs");
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require('openai');
+const fs = require('fs');
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,45 +8,75 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 function getDefaultPrompt(title, question) {
-  return `Думай как ${title}. Задавай встречный вопрос и старайся вести дискуссию со мной.
-
+  return `Думай как ${title}. Cтарайся вести дискуссию со мной.
   Не нужно в начале писать что ты искусственный интеллект.
 
   Ответь на вопрос "${question}", а также создай встречный вопрос на эту тему.`;
 }
 
 (async function () {
-  let question = "Чей Крым?";
-  let title1 = "Ватник";
-  let title2 = "Либерал";
+  let question = 'Чей Крым?';
 
-  const req = async function (title, question) {
+  const personality1 = {
+    title: 'Ватник',
+    history: [],
+    promptGenerator: getDefaultPrompt,
+  };
+  const personality2 = {
+    title: 'Либерал',
+    history: [],
+    promptGenerator: getDefaultPrompt,
+  };
+
+  const req = async function (personality, question) {
     const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
+      model: 'gpt-3.5-turbo',
       max_tokens: 400,
       temperature: 0.5,
       messages: [
+        ...personality.history,
         {
-          content: getDefaultPrompt(title, question),
-          role: "user",
+          content: personality.promptGenerator(personality.title, question),
+          role: 'user',
         },
       ],
     });
     const answer = completion.data.choices[0].message.content.replaceAll(
-      "\n",
-      " "
+      '\n',
+      ' '
     );
-    console.log(`- ${title}: ${answer.toString()}`);
-    fs.appendFileSync("data.txt", `- ${title}: ${answer.toString()}\n`);
+    console.log(`- ${personality.title}: ${answer}`);
+    fs.appendFileSync('data.txt', `- ${personality.title}: ${answer}\n`);
+
+    const another =
+      personality.title === personality1.title ? personality2 : personality1;
+    const me =
+      personality.title === personality1.title ? personality1 : personality2;
+
+    me.history.push({
+      role: 'assistant',
+      content: answer,
+    });
+    another.history.push({
+      role: 'user',
+      content: answer,
+    });
+
+    const maxHistory = parseInt(process.env.MAX_HISTORY);
+    if (me.history.length > maxHistory) {
+      me.history.shift();
+      another.history.shift();
+    }
+
+    const delay = parseInt(process.env.REQUESTS_DELAY);
 
     return new Promise((resolve, reject) => {
-      let t = 0;
       setTimeout(async () => {
-        const data = await req(title === title1 ? title2 : title1, answer);
+        const data = await req(another, answer);
         resolve(data);
-      }, 10000);
+      }, delay * 1000);
     });
   };
 
-  await req(title1, question);
+  await req(personality1, question);
 })();
